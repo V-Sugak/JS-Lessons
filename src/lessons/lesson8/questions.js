451832739191244193fc91384971eff25851569c
V-Sugak <vicka.sugak2010@yandex.ru>
2022-04-25 21:41:04 +0300

    Tasks

--- src/lessons/lesson8/questions.js
+++ src/lessons/lesson8/questions.js
@@ -98,3 +98,104 @@ console.log(c)
 // }
 // инициализация url на глобальном уравне, т.к. происходит присвоение
 
+//Task 4
+// Какие ключи будут заполнены у pet1
+// function Animal(size, type) {
+// 	this.type = type || 'Animal';
+// 	this.canTalk = false;
+// }
+// Animal.prototype.speak = function() {
+// 	if (this.canTalk) {
+// 		console.log('It spoke!');
+// 	}
+// }
+//
+// let Pet = function(size, type, name, owner) {
+// 	Animal.call(this, size, type);
+// 	this.size = size;
+// 	this.name = name;
+// 	this.owner = owner;
+// }
+//
+// Pet.prototype = Object.create(Animal.prototype);
+// let pet1 = new Pet();
+//console.log(pet1)
+// Result: {type:'Animal', canTalk: false, size: undefined,name:undefined, owner:undefined}, speak in prototype
+
+
+//Task 5
+// let array = [1,2,3,4,4,5,4,4];
+// for (let i = 0; i < array.length; i++) {
+// 	if (array[i] === 4){
+// 		array.splice(i,1);
+// 	}
+// }
+// console.log(array);            // [1,2,3,4,5,4]
+
+//Task 5a
+// function Monster(){};
+// Monster.prototype.growl = () => console.log('Grr!');
+//
+// console.log(new Monster().growl === new Monster().growl);           // true
+
+//Task 6
+// let first = 'Who';
+// let second = 'What';
+// try {
+// 	try {
+// 		throw new Error('Sad trombone');
+// 	} catch (err) {
+// 		first = 'Why';
+// 		throw err;
+// 	} finally {
+// 		second = 'When';
+// 	}
+// } catch (err) {
+// 	second = 'Where';
+// }
+//
+// console.log(first);            //Why
+// console.log(second);           //Where
+
+//Task 7
+// setTimeout(() => console.log(1), 0);
+// console.log(2);
+// new Promise((resolve, reject) => {
+// 	setTimeout(() => reject(console.log(3), 1000));
+// }).catch(() => console.log(4));
+// console.log(5);
+// 2 5 1 3 4
+
+//Task 8
+let date = new Date(2020,05,10);
+let dateDusplayOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+let formattedDate = date.toLocaleDateString('en', dateDusplayOptions);
+console.log(formattedDate);
+// June 10, 2020
+
+
+//Task 9
+let objBook = {title: 'JavaScript',};
+Object.preventExtensions(objBook);
+let newObjBook = objBook;
+newObjBook.author = 'Robert';
+
+console.log(objBook);       // {title: 'JavaScript'}
+console.log(newObjBook);   // {title: 'JavaScript'}
+
+//Task 10
+function getAvaliabilityMessage(item) {
+    if (getAvailability(item)) {
+        var msg = 'Username avaliable';
+    }
+    return msg;
+}
+
+function getAvailability(name) {
+    return name === 'Eugene';
+}
+console.log('getAvaliabilityMessage ', getAvaliabilityMessage('newUserName'));  //undefined
